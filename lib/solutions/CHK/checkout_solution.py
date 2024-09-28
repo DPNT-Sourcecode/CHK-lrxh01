862b0db8f70dfd6fd23394324e866123f5e49285
sbx_user1051 <sbx_user1051@169.254.52.173>
2024-09-28 11:56:11 +0000

    Sat Sep 28 11:56:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,45 +9,43 @@ def checkout(skus):
 
     if skus == "":
         return 0
-    re.match(r'^[ABCD]+$',skus).start()
+    
     try: 
         re.match(r'^[ABCD]+$',skus).start()
-        return -1
     except:
+        return -1
         
-        total = 0
-        prices = {
-            "A":50,
-            "A_deal":130,
-            "A_deal_ammount":3,
-            "B":30,
-            "B_deal":45,
-            "B_deal_ammount":2,
-            "C":20,
-            "D":15,
-            }
+    total = 0
+    prices = {
+        "A":50,
+        "A_deal":130,
+        "A_deal_ammount":3,
+        "B":30,
+        "B_deal":45,
+        "B_deal_ammount":2,
+        "C":20,
+        "D":15,
+        }
 
-        
-        number_of_a = skus.count('A')
-        number_of_b = skus.count('B')
-        number_of_b = 0
-        number_of_c = skus.count('C')
-        number_of_c = 0
-        number_of_d = skus.count('D')
-        number_of_d = 0
+    
+    number_of_a = skus.count('A')
+    number_of_b = skus.count('B')
+    number_of_c = skus.count('C')
+    number_of_d = skus.count('D')
 
 
-        value_of_a = floor(number_of_a/prices['A_deal_ammount'])*prices["A_deal"] + (number_of_a % prices["A_deal_ammount"])*prices['A']
-        value_of_b= floor(number_of_b/prices['B_deal_ammount'])*prices["B_deal"] + (number_of_b % prices["B_deal_ammount"])*prices['B']
-        value_of_c =  number_of_c * prices['C']
-        value_of_d =  number_of_d *  prices['D'] 
+    value_of_a = floor(number_of_a/prices['A_deal_ammount'])*prices["A_deal"] + (number_of_a % prices["A_deal_ammount"])*prices['A']
+    value_of_b= floor(number_of_b/prices['B_deal_ammount'])*prices["B_deal"] + (number_of_b % prices["B_deal_ammount"])*prices['B']
+    value_of_c =  number_of_c * prices['C']
+    value_of_d =  number_of_d *  prices['D'] 
 
-        total = value_of_a + value_of_b + value_of_c + value_of_d
+    total = value_of_a + value_of_b + value_of_c + value_of_d
 
-        if total == 0:
-            return -1
+    if total == 0:
+        return -1
 
-        return total
+    return total
+
 
 
 
