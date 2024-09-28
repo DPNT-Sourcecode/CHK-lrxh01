f98d2a047ae0bc823fdfcc8b114c63798e77f694
sbx_user1051 <sbx_user1051@169.254.52.173>
2024-09-28 11:28:39 +0000

    Sat Sep 28 11:28:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,7 +2,40 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from math import floor
 def checkout(skus):
 
+    total = 0
+    prices = {
+        "A":50,
+        "A_deal":130,
+        "A_deal_ammount":3,
+        "B":30,
+        "B_deal":45,
+        "B_deal_ammount":2,
+        "C":20,
+        "D":15,
+        }
 
-    return 50
+    try:
+        number_of_a = skus.count('A')
+    except:
+        number_of_a = 0
+    try:
+        number_of_b = skus.count('B')
+    except:
+        number_of_b = 0
+    try:
+        number_of_c = skus.count('C')
+    except:
+        number_of_c = 0
+    try:
+        number_of_d = skus.count('D')
+    except:
+        number_of_d = 0
+
+
+    value_of_a = floor(number_of_a/prices['A_deal_ammount'])*prices["A_deal"] + (number_of_a % prices["A_deal_ammount"])*prices['A']
+
+    return value_of_a
+
