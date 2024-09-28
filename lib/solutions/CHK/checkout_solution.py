32e94a06a8a632b76799382aa89e3ba8ef10de5e
sbx_user1051 <sbx_user1051@169.254.116.141>
2024-09-28 14:48:33 +0000

    Sat Sep 28 14:48:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,8 +15,8 @@ def get_value(number,price, small_deal_price=None, small_deal_quantity=None, big
 
     else:
         not_free = 0
-
-    if trigger_value != None and number >= trigger_for_free:
+    print(number,trigger_for_free,trigger_value)
+    if trigger_value != None:
         free = floor((trigger_value - not_free)/trigger_for_free)
         print(free)
         number = number - free
@@ -100,6 +100,7 @@ def checkout(skus):
     print(values)
 
     return total
+
 
 
 
