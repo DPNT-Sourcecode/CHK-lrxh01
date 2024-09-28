142695cdc9319f10707816ebaa96c41e559ab090
sbx_user1051 <sbx_user1051@169.254.67.13>
2024-09-28 09:41:57 +0000

    Sat Sep 28 09:41:57 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """Returns sum of two integers between 0 and 100"""
+
+    test_for_valid_input = x >= 0 and x <= 100 and y >= 0 and y <=100
+
+    if test_for_valid_input:
+        return x + y
+    else:
+        return None
+    
+
