9af849d6392ed0f3f606fbaca5874f0a4736491e
sbx_user1051 <sbx_user1051@169.254.52.173>
2024-09-28 11:31:39 +0000

    Sat Sep 28 11:31:39 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,3 +3,6 @@ from lib.solutions.CHK import checkout_solution
 class TestCheckout():
     def test_checkout(self):
         assert checkout_solution.checkout('A') == 50
+
+    def test_lots_of_A(self):
+        assert checkout_solution.checkout('AAAA') == 180
