0d463526d71539c037c9980d020d90eedce69b72
sbx_user1051 <sbx_user1051@169.254.116.141>
2024-09-28 14:22:59 +0000

    Sat Sep 28 14:22:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,8 +19,9 @@ def get_value(number,price, small_deal_price=None, small_deal_quantity=None, big
 
     else:
         free = 0
+    print(number)
     number = number - free
-
+    print(number)
     if small_deal_price==None:
         return number*price
     elif small_deal_price and big_deal_quantity==None:
@@ -63,39 +64,9 @@ def checkout(skus):
         'trigger_for_free':None,'trigger_value':None,'min_basket':None},
         {'Item':'F','Price':10,'small_deal_price':None,'small_deal_quantity':None,'big_deal_price':None,'big_deal_quantity':None,
         'trigger_for_free':2,'trigger_value':"F",'min_basket':3},
-        {'Item':'C','Price':20,'small_deal_price':None,'small_deal_quantity':None,'big_deal_price':None,'big_deal_quantity':None,
-        'trigger_for_free':None,'trigger_value':None,'min_basket':None},
-        {'Item':'C','Price':20,'small_deal_price':None,'small_deal_quantity':None,'big_deal_price':None,'big_deal_quantity':None,
-        'trigger_for_free':None,'trigger_value':None,'min_basket':None},
-        {'Item':'C','Price':20,'small_deal_price':None,'small_deal_quantity':None,'big_deal_price':None,'big_deal_quantity':None,
-        'trigger_for_free':None,'trigger_value':None,'min_basket':None},
-        {'Item':'C','Price':20,'small_deal_price':None,'small_deal_quantity':None,'big_deal_price':None,'big_deal_quantity':None,
-        'trigger_for_free':None,'trigger_value':None,'min_basket':None},
-        {'Item':'C','Price':20,'small_deal_price':None,'small_deal_quantity':None,'big_deal_price':None,'big_deal_quantity':None,
-        'trigger_for_free':None,'trigger_value':None,'min_basket':None},
-        {'Item':'C','Price':20,'small_deal_price':None,'small_deal_quantity':None,'big_deal_price':None,'big_deal_quantity':None,
-        'trigger_for_free':None,'trigger_value':None,'min_basket':None},
 
         ]
-    total = 0
-    prices = {
-        "A":50,"A_deal":130,"A_deal_ammount":3,"A_second_deal":200,"A_second_amount":5,
-        "B":30,"B_deal":45, "B_deal_ammount":2,
-        "C":20,
-        "D":15,
-        "E":40,
-        "F":10, "F_deal_amount":2, "F_deal":10,
-        }
-    
-    
-    
-    number_of_a = skus.count('A')
-    number_of_b = skus.count('B')
-    number_of_c = skus.count('C')
-    number_of_d = skus.count('D')
-    number_of_e = skus.count('E')
-    number_of_f = skus.count('F')
-
+    total = 0  
     
     values = [
         get_value(number=skus.count(item['Item']),price=item['Price'],small_deal_price=item['small_deal_price'],small_deal_quantity=item['small_deal_quantity'],
@@ -103,34 +74,9 @@ def checkout(skus):
                   trigger_value=count_func(skus,item['trigger_value']),min_basket=item['min_basket']) 
         for item in grid
         ]
-    
 
-    value_of_b = get_value(number=number_of_b,price=prices['B'],small_deal_price=45,small_deal_quantity=2,
-                           trigger_for_free=2,trigger_value=number_of_e)
-
-    value_of_c =  get_value(number_of_c,prices["C"])
-    value_of_d =  get_value(number_of_c,prices['D'])
-    value_of_e = (number_of_e ) * prices["E"]
-
-    # Once you go past the 3 F mark you get the 1st F for free as well
-    if number_of_f >= 3:
-        value_of_f = (floor((number_of_f-1)/prices['F_deal_amount'])*prices["F_deal"] 
-                      + ((number_of_f-1) % prices["F_deal_amount"])*prices['F']
-                      +prices['F']) 
-    else:
-        value_of_f = number_of_f * prices['F']
 
     total = sum(values)
-
-    if total == 0:
-        return -1
+    print(values)
 
     return total
-
-
-
-
-
-
-
-
