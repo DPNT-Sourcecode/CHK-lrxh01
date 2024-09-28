46459fa0b52acf648d3eb90d22cd5b22620afbbf
sbx_user1051 <sbx_user1051@169.254.116.141>
2024-09-28 12:12:16 +0000

    Sat Sep 28 12:12:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,11 +20,14 @@ def checkout(skus):
         "A":50,
         "A_deal":130,
         "A_deal_ammount":3,
+        "A_second_deal":200,
+        "A_second_amount":5,
         "B":30,
         "B_deal":45,
         "B_deal_ammount":2,
         "C":20,
         "D":15,
+        "E":40,
         }
 
     
@@ -32,16 +35,19 @@ def checkout(skus):
     number_of_b = skus.count('B')
     number_of_c = skus.count('C')
     number_of_d = skus.count('D')
+    number_of_e = skus.count('E')
 
 
     value_of_a = floor(number_of_a/prices['A_deal_ammount'])*prices["A_deal"] + (number_of_a % prices["A_deal_ammount"])*prices['A']
     value_of_b= floor(number_of_b/prices['B_deal_ammount'])*prices["B_deal"] + (number_of_b % prices["B_deal_ammount"])*prices['B']
     value_of_c =  number_of_c * prices['C']
     value_of_d =  number_of_d *  prices['D'] 
+    value_of_e = (number_of_e - floor(number_of_b/2)) * prices["E"]
 
-    total = value_of_a + value_of_b + value_of_c + value_of_d
+    total = value_of_a + value_of_b + value_of_c + value_of_d + value_of_e
 
     if total == 0:
         return -1
 
     return total
+
