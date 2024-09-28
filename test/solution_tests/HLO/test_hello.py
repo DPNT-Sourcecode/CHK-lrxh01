f90c45ea8d173fe647c4ef462d4b7a51bc610db0
sbx_user1051 <sbx_user1051@169.254.24.105>
2024-09-28 11:06:40 +0000

    Sat Sep 28 11:06:40 UTC 2024

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -5,5 +5,6 @@ from lib.solutions.HLO import hello_solution
 class TestHello():
     def test_sum(self):
         
-        assert hello_solution.hello('Test string') == "Hello, World!"
+        assert hello_solution.hello('John') == "Hello, John!"
+
 
